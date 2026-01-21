a8c627e899a6364b83919823bf4dc66a7272fc6c
Chad Malone Murphy <130190686+ChadM95@users.noreply.github.com>
2026-01-21 15:52:11 +0000

    add users controller

---
+++ src/main/java/com/example/MyApp/controllers/UserController.java
@@ -0,0 +1,15 @@
+package com.example.MyApp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/users")
+    public String getUsers() {
+        return "returns users";
+    }
+
+
+}
