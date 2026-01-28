137eba65fee032de4fa39bcb1cf563a95721f66a
Chad Malone Murphy <130190686+ChadM95@users.noreply.github.com>
2026-01-28 11:57:56 +0000

    add user entity class

--- src/main/java/com/example/MyApp/controller/InfoController.java
+++ src/main/java/com/example/MyApp/controller/InfoController.java
@@ -1,4 +1,4 @@
-package com.example.MyApp.controllers;
+package com.example.MyApp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
---
+++ src/main/java/com/example/MyApp/entity/User.java
@@ -0,0 +1,14 @@
+package com.example.MyApp.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 200)
+    private String name;
+}
