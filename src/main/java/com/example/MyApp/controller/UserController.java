747ef6c677e9c1f7779a6caad97f7e2fb69e4213
Chad Malone Murphy <130190686+ChadM95@users.noreply.github.com>
2026-02-04 14:10:36 +0000

    add crud methods to user controller

--- src/main/java/com/example/MyApp/controller/UserController.java
+++ src/main/java/com/example/MyApp/controller/UserController.java
@@ -11,6 +11,41 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepo;
 
+    // create / post
+    @PostMapping
+    public User addUser(@RequestBody User u) {
+        return userRepo.save(u);
+    }
+
+    // read
+    // get all
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepo.findAll();
+    }
+    //get by id
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userRepo.findById(id).orElse(null);
+    }
+
+    // update / put
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User newData) {
+        return userRepo.findById(id).map(user -> {
+            user.setName(newData.getName());
+            user.setSurname(newData.getSurname());
+            return userRepo.save(user);
+        }).orElse(null);
+    }
+
+    // delete
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userRepo.deleteById(id);
+    }
 
 }
